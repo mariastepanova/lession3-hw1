0bc8b4938f0881f69fa53a5c29b39b676b06fe9b
vyacheslav Potravnyy <vpotravnyy@intersog.com>
2015-07-08 17:49:24 +0300

    Added styles and tests

---
+++ js/main.js
@@ -0,0 +1,156 @@
+/**
+ * chunk(array, size);
+ *
+ * Разделяет массив на куски по size элементов
+ * В последнем куске может быть меньше элементов чем size
+ *
+ * @param {Array} Массив для обработки.
+ * @param {number} Длина одного куска.
+ * @returns {Array} массив, содержащий куски
+ *
+ * @example:
+ * chunk(['a', 'b', 'c', 'd'], 2); // => [['a', 'b'], ['c', 'd']]
+ * chunk(['a', 'b', 'c', 'd'], 3); // => [['a', 'b', 'c'], ['d']]
+ */
+function chunk(array, size) {
+
+}
+
+
+/**
+ * flatten(array);
+ *
+ * Выравнивет вложенный массив в невложенный.
+ * То есть в итоговом массиве других массивов быть не может
+ *
+ * @param {Array} Массив для обработки.
+ * @returns {Array} Плоский массив
+ *
+ * @example:
+ * flatten([1, [2, 3, [4]]]); // => [1, 2, 3, 4]
+ */
+function flatten(array) {
+
+}
+
+
+/**
+ * intersection(arr1, arr2, arr3, ...);
+ *
+ * Возвращает массив со значениями, встречающимися во всех входящих массивах
+ *
+ * @param {...Array} Массивы для обработки
+ * @returns {Array} Массив-пересечение
+ *
+ * @example:
+ * intersection([1, 2], [4, 2], [2, 1]) // → [2]
+ */
+function intersection() {
+
+}
+
+
+/**
+ * remove(array, predicate);
+ *
+ * Удаляет из массива все элементы, вызов предиката для которых возвращает true
+ *
+ * @param {Array} Массив для обработки
+ * @param {Function} Предикат, принимает элемент массива, возвращает boolean
+ * @returns {Array} Массив без удаленных элементов.
+ *
+ * @example:
+ * remove([1, 2, 3, 4], function(n) {return n % 2 == 0}); // → [1, 3]
+ */
+function remove(array, predicate) {
+
+}
+
+
+/**
+ * uniq(array);
+ *
+ * Удаляет из массива повторенные элементы
+ *
+ * @param {Array} Массив для обработки
+ * @returns {Array} Массив без удаленных элементов.
+ *
+ * @example:
+ * uniq([2, 1, 2]) // → [2, 1]
+ */
+function uniq(array) {
+
+}
+
+
+/**
+ * union(arr1, arr2, arr3, ...);
+ *
+ * Объединяет массивы, но не включая повторы элементов
+ *
+ * @param {...Array} Массивы для обработки
+ * @returns {Array} Объединенный массив без повторов.
+ *
+ * @example:
+ * union([1, 2], [4, 2], [2, 1]); // → [1, 2, 4]
+ */
+function union() {
+
+}
+
+
+/**
+ * union(arr1, arr2, arr3, ...);
+ *
+ * Создает массив сгруппированных элементов, в котором первый массив - 
+ * это массив первых элементов входящих массивов, и т.д.
+ *
+ * @param {...Array} Массивы для группировки
+ * @returns {Array} Массив с перегруппированными массивами.
+ *
+ * @example:
+ * zip(['fred', 'barney'], [30, 40], [true, false]); // → [['fred', 30, true], ['barney', 40, false]]
+ */
+function zip() {
+
+}
+
+
+
+// Testing
+console.log(
+    "chunk        ",
+    chunk([1,2,3,4], 2)
+);
+
+console.log(
+    "flatten      ",
+    flatten([1, [2, 3, [4]]])
+);
+
+console.log(
+    "intersection ",
+    intersection([1, 2], [4, 2], [2, 1]) 
+);
+
+console.log(
+    "remove       ",
+    remove([1, 2, 3, 4], function(n) {
+        return n % 2 == 0
+    })
+);
+
+console.log(
+    "uniq         ",
+    uniq([2, 1, 2]) 
+);
+
+console.log(
+    "union        ",
+    union([1, 2], [4, 2], [2, 1])
+);
+
+console.log(
+    "zip          ",
+    zip(['fred', 'barney'], [30, 40], [true, false])
+);
